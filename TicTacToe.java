2e9c285b4c0161e1f7c81cb8f79481170bf7e9fe
Bikash Kumar Muni <91220508+Bikash0007@users.noreply.github.com>
2023-10-04 23:52:12 +0530

    Create TicTacToe.java

---
+++ TicTacToe.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class TicTacToe {
+    private char[][] board;
+    private char currentPlayer;
+
+    public TicTacToe() {
+        board = new char[3][3];
+        currentPlayer = 'X';
+        initializeBoard();
+    }
+
+    public void initializeBoard() {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                board[i][j] = '-';
+            }
+        }
+    }
+
+    public void printBoard() {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                System.out.print(board[i][j] + " ");
+            }
+            System.out.println();
